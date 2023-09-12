b02513c84c0ae391f72a2a8281d15df5f6276eb4
Jam19th <jam19th@gmail.com>
2023-09-12 11:30:50 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Seed_Data from "./Seed_Data";
 
 function App() {
   return (
@@ -6,7 +7,23 @@ function App() {
         <a href="/">Amazona</a>
       </header>
       <main>
-        List of Products
+        <h1> Featured Products</h1>
+        <div className="products">
+          {Seed_Data.products.map(product => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
